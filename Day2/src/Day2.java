5603c54066c9dbbc38d5e73b1c381ce6ae2d4cfa
bhusalsa <156117237+bhusalsa@users.noreply.github.com>
2025-09-27 18:56:21 -0700

    Updated the previous methods to use the new helper methods and Completed the Part 2 of Day 2

--- Day2/src/Day2.java
+++ Day2/src/Day2.java
@@ -3,18 +3,16 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Day2 {
-    static String filename = "Day2/resources/test.txt";
+    static String filename = "Day2/resources/report.txt";
     static ArrayList<int[]> unsafeP1 = new ArrayList<>();
-    static ArrayList<Integer> issuelocations = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         StringBuilder report = parser(filename);
         String[] lines = report.toString().split(System.lineSeparator());
 
         int safe = checkReport(lines);
+        System.out.println();
         int dampened = dampner(unsafeP1);
 
         System.out.println("Safe reports: " + safe);
@@ -38,8 +36,6 @@ public class Day2 {
 
     public static int checkReport(String[] lines){
         int safe = 0;
-        int i = 0;
-        int damp = 0;
 
         for (String line : lines) {
             String[] values = line.split(" ");
@@ -49,91 +45,102 @@ public class Day2 {
                 value[k] = Integer.parseInt(values[k]);
             }
 
-            boolean ok = true;
-            if (value[0] > value[1]) {
-                for (int k = 1; k < value.length; k++) {
-                    if (value[k - 1] > value[k]) {
-                        int diff = Math.abs(value[k - 1] - value[k]);
-                        if (diff >= 1 && diff <= 3) {
-                            continue;
-                        }
-                    }
-                    unsafeP1.add(value);
-                    issuelocations.add(k-1);
-                    ok = false;
-                    break;
-                }
-            } else if (value[0] < value[1]) {
-                for (int k = 1; k < value.length; k++) {
-                    if (value[k - 1] < value[k]) {
-                        int diff = Math.abs(value[k - 1] - value[k]);
-                        if (diff >= 1 && diff <= 3) {
-                            continue;
-                        }
-                    }
-                    unsafeP1.add(value);
-                    issuelocations.add(k-1);
-                    ok = false;
-                    break;
+            //check each rule separately
+            boolean ok = false;
+            boolean increasing = checkIncreasing(value);
+            boolean decreasing = checkDecreasing(value);
+            boolean difference = checkDifference(value);
 
-                }
-            } else {
-                unsafeP1.add(value);
-                ok = false;
+            //decide whether it follows the rules
+            if ((increasing && difference) || (decreasing && difference)) {
+                ok = true;
             }
+
             if (ok) {
                 safe++;
                 System.out.println(line);
-            }
-
+            }else {unsafeP1.add(value);}
         }
-
         return safe;
     }
 
     public static int dampner(ArrayList<int[]> unsafeP1) {
-        int count = 0;
+
+        int increasingError = 0;
+        int decreasingError = 0;
+        int differenceError = 0;
+        int dampened = 0;
+
+        //remove the value causing problem
         for (int i = 0; i < unsafeP1.size(); i++) {
             int[] values = unsafeP1.get(i);
-            int prev =  values[0];
-            boolean ok = true;
 
-            int removed = 0;
-            for (int j = 1; j < values.length; j++) {
-                if (values[j] > prev) {
-                    prev = values[j];
+            //check whether the array is showing increasing or decreasing pattern +1 = increasing -1= decreasing
+            int trend = trend(values);
+            int error = -1;
+
+            //if increasing check error in increasing
+            if(trend == 1) {
+                error = checkIncreasingError(values);
+            }else if(trend == -1) { //check decreasing error
+                error = checkDecreasingError(values);
+            }
+
+            //if both increasing and decreasing don't find an error check for error in difference
+            if(error == -1) {
+            error = checkDifferenceError(values);
+            }
+
+            //error check on difference is done second as we can only fix one error if there is an error
+            // already in increasing or decreasing sequence then we don't need to find another error in difference,
+            // we are only looking for 1 error
+
+            //error might be in i or i-1 so create two arrays and check both
+            int[] dampenedArrayi =  new int[values.length-1];
+            int[] dampenedArrayi1 = new int[values.length-1];
+            int position = 0;
+            int position1 = 0;
+
+            //create two array by removing error and error-1 respectively
+            for (int j = 0; j < values.length; j++) {
+                if (j == error) {
                     continue;
                 }
-                removed++;
-                prev = values[j-1];
-
-                if (removed > 1) {
-                    ok = false;
-                    break;
+                dampenedArrayi[position++] = values[j];
+            }
+            for (int j = 0; j < values.length; j++) {
+                if (j == error - 1 ) {
+                    continue;
                 }
-            }
-            if(!ok) {
-                for (int j = 1; j < values.length; j++) {
-                    if (values[j] < prev) {
-                        prev = values[j];
-                        continue;
-                    }
-                    removed++;
-                    prev = values[j - 1];
-
-                    if (removed > 1) {
-                        ok = false;
-                        break;
-                    }
-                }
-            }
-            if (ok) {
-                printArray(values);
-                count++;
-            }
+                dampenedArrayi1[position1++] = values[j];
             }
 
-        return count;
+            //check if they are now correct
+            boolean dampenedi = checkDampenedReport(dampenedArrayi);
+            boolean dampednedi1 = checkDampenedReport(dampenedArrayi1);
+
+            //if either one of the array is correct increase the dampened number
+            if(dampenedi || dampednedi1){
+                dampened++;
+            }
+        }
+
+        //return the total number of dampened arrays
+        return dampened;
+    }
+
+    public static boolean checkDampenedReport(int[] line) {
+
+            boolean ok = false;
+            boolean increasing = checkIncreasing(line);
+            boolean decreasing = checkDecreasing(line);
+            boolean difference = checkDifference(line);
+            if ((increasing && difference) || (decreasing && difference)) {
+                //printArray(line);
+                return true;
+            }
+
+        return false;
     }
 
     public static void printArray(int[] array) {
@@ -141,5 +148,87 @@ public class Day2 {
             System.out.print(j + " ");
         }
         System.out.println();
+    }
+
+    public static boolean checkIncreasing(int[] values){
+        boolean increasing = true;
+        for (int i = 1; i < values.length; i++) {
+            if (values[i-1] > values[i]) {
+                return false;
+            }
+        }
+        return increasing;
+    }
+
+    public static boolean checkDecreasing(int[] values){
+        boolean decreasing = true;
+        for (int i = 1; i < values.length; i++) {
+            if (values[i-1] < values[i]) {
+                return false;
+            }
+        }
+        return decreasing;
+    }
+
+    public static boolean checkDifference(int[] values){
+        boolean difference = true;
+        for (int i = 1; i < values.length; i++) {
+            int diff = Math.abs(values[i-1] - values[i]);
+            if (diff < 1 || diff >3) {
+                return false;
+            }
+        }
+        return difference;
+    }
+
+    public static int checkIncreasingError(int[] values){
+        boolean increasing = true;
+        for (int i = 1; i < values.length; i++) {
+            if (values[i-1] > values[i]) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static int checkDecreasingError(int[] values){
+        for (int i = 1; i < values.length; i++) {
+            if (values[i-1] < values[i]) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static int checkDifferenceError(int[] values){
+
+        for (int i = 1; i < values.length; i++) {
+            int diff = Math.abs(values[i-1] - values[i]);
+            if (diff < 1 || diff >3) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static int trend(int[] values){
+        int increase = 0;
+        int decrease = 0;
+        int previous = values[0];
+
+        //loop thru all the values and check if there are more
+        // increasing or decreasing values to decide the trend
+        for (int i = 1; i < values.length; i++) {
+            int current = values[i];
+            if (current > previous) {
+                increase++;
+            }else if (current < previous) {
+                decrease++;
+            }
+            previous = current;
+        }
+
+        //return +1 or -1 based on whether increasing or decreasing
+        return (increase > decrease) ? +1 : -1;
     }
 }
